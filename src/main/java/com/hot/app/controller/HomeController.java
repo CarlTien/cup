5f1b3e65b29727c241a26d1e03370046bed1b5c8
carltien <carltien@mail.com>
2016-08-26 07:15:51 +0800

    remote ip

--- src/main/java/com/hot/app/controller/HomeController.java
+++ src/main/java/com/hot/app/controller/HomeController.java
@@ -1,13 +1,19 @@
 package com.hot.app.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
-	@RequestMapping("/home")
-	public String home() {
-		return "home";
+	@RequestMapping(value = "/home", method = RequestMethod.GET)
+	public String home(HttpServletRequest request) {
+
+		return "request.getRemoteAddr():" + request.getRemoteAddr() + ",getLocalAddr():" + request.getLocalAddr()
+				+ ",getLocalPort():" + request.getLocalPort() + ",org.apache.tomcat.remoteAddr:"
+				+ request.getAttribute("org.apache.tomcat.remoteAddr");
 	}
 }
